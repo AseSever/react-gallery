a2206b42b4ed869a27f296b3f3c5979aa06c3451
Alex Severson <alexsvrsn@gmail.com>
2020-08-15 13:27:00 -0500

    added base mode component files and added some pictures

---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class GalleryList extends Component {
+
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default GalleryList;
