646bcecc947b6655a1ac1a32f6c365905fcd3bf6
Alex Severson <alexsvrsn@gmail.com>
2020-08-15 13:52:35 -0500

    GalleryList is functioning with GET request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
 
 class App extends Component {
+
+  state = {
+    gallery: []
+  }
+
+  componentDidMount() {
+    this.getGalleryItems();
+  }
+
+  getGalleryItems = () => {
+    axios.get('/gallery')
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          gallery: response.data
+        })
+      })
+      .catch(error => {
+        alert('Error in GET request')
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +34,7 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList gallery={this.state.gallery}/>
       </div>
     );
   }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -4,7 +4,13 @@ class GalleryList extends Component {
 
     render() {
         return (
-            
+            <div class="galleryList">
+                {this.props.gallery.map(galleryItem => {
+                    return (
+                        <img src={galleryItem.path} alt=""/>
+                    )
+                })}
+            </div>
         )
     }
 }
